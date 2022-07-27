8ce0736fae1cdda230cdce87101fec88be58d9c0
normie <aragonisafferz@gmail.com>
2022-07-27 18:35:37 +0700

    added a os and revised player file

---
+++ oses/m95.py
@@ -0,0 +1,9 @@
+class system():
+    name = "Mail95"
+    shortname = "M95"
+    prolevel = 10
+    unlocklevel = 0
+    systemunlock = "M95plus"
+    requiredstring = ""
+    listinbootmenu = 0
+    startupstring = "M a i l 9 5"
